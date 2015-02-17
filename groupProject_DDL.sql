501e7a721cb18169ca785826ed5ea0e8c23ef022
MatthewWiper <w0281488@nscc.ca>
2015-02-17 17:36:18 -0400

    More error fixes

--- groupProject_DDL.sql
+++ groupProject_DDL.sql
@@ -17,7 +17,8 @@ DROP TABLE nscc_Payment_Cheque;
 DROP TABLE nscc_Payment_Credit_Card;
 DROP TABLE nscc_Credit_Card_Company;
 DROP TABLE nscc_Campus;
-CREATE TABLE nscc_Applicant(
+CREATE TABLE nscc_Applicant
+(
 Applicant_ID integer PRIMARY KEY,
 SIN char(9),
 FirstName varchar(30) NOT NULL,
@@ -54,10 +55,10 @@ FirstLanguage_ID integer NOT NULL FOREIGN KEY REFERENCES nscc_Language(Language_
 FirstLanguage_Other varchar(50)
 );
 CREATE TABLE nscc_School_Type
-{
+(
 School_Type_ID integer NOT NULL PRIMARY KEY,
 Type_Name varchar(50) NOT NULL
-};
+);
 CREATE TABLE nscc_Education
 (
 Education_ID integer NOT NULL PRIMARY KEY,
@@ -67,62 +68,74 @@ LocationAddress varchar(50),
 School_Type_ID integer NOT NULL FOREIGN KEY REFERENCES nscc_School_Type(School_Type_ID),
 Applicant_ID integer NOT NULL FOREIGN KEY REFERENCES nscc_Applicant(Applicant_ID)
 );
-CREATE TABLE nscc_Language(
+CREATE TABLE nscc_Language
+(
 Language_ID integer NOT NULL PRIMARY KEY,
 Language_Name varchar(50) NOT NULL
 );
-CREATE TABLE nscc_Citizenship(
+CREATE TABLE nscc_Citizenship
+(
 Citizenship_ID integer NOT NULL PRIMARY KEY,
 Description varchar(100) NOT NULL
 );
-CREATE TABLE nscc_Phone(
+CREATE TABLE nscc_Phone
+(
 Phone_ID integer NOT NULL PRIMARY KEY,
 Phone_Number varchar(20) NOT NULL,
 Is_Primary char(1) NOT NULL
 );
-CREATE TABLE nscc_Province_State(
+CREATE TABLE nscc_Province_State
+(
 Province_State_Code char(2) NOT NULL PRIMARY KEY,
 Country_Code char(2) NOT NULL,
 Province_State_Name varchar(50) NOT NULL
 );
-CREATE TABLE nscc_Country(
+CREATE TABLE nscc_Country
+(
 Country_Code char(2) NOT NULL PRIMARY KEY,
 Country_Name varchar(50) NOT NULL
 );
-CREATE TABLE nscc_Program(
+CREATE TABLE nscc_Program
+(
 Program_ID integer NOT NULL PRIMARY KEY,
 Program_Name varchar(100) NOT NULL,
 Active integer NOT NULL
 );
-CREATE TABLE nscc_Application(
+CREATE TABLE nscc_Application
+(
 Application_ID integer NOT NULL PRIMARY KEY,
 Submit_Date date NOT NULL,
 Application_Fee number(7,2),
 Paid integer NOT NULL,
 Applicant_ID integer NOT NULL FOREIGN KEY REFERENCES nscc_Applicant(Applicant_ID)
 );
-CREATE TABLE nscc_Payment_Money_Order(
+CREATE TABLE nscc_Payment_Money_Order
+(
 Payment_ID integer NOT NULL,
 Money_Order_Number varchar(20) NOT NULL
 );
-CREATE TABLE nscc_Payment_Cheque(
+CREATE TABLE nscc_Payment_Cheque
+(
 Payment_ID integer NOT NULL,
 Cheque_Number varchar(20) NOT NULL,
 Transit_Number varchar(20) NOT NULL,
 Financial_Institution varchar(20) NOT NULL,
 Account_Number varchar(20) NOT NULL
 );
-CREATE TABLE nscc_Campus(
+CREATE TABLE nscc_Campus
+(
 Campus_ID integer NOT NULL PRIMARY KEY,
 Campus_Name varchar(50) NOT NULL
 );
-CREATE TABLE nscc_Payment(
+CREATE TABLE nscc_Payment
+(
 Payment_ID integer NOT NULL PRIMARY KEY,
 Application_ID integer NOT NULL FOREIGN KEY REFERENCES nscc_Application(Application_ID),
 Payment_Date date NOT NULL,
 Amount number(7,2) NOT NULL
 );
-CREATE TABLE nscc_Program_Choice(
+CREATE TABLE nscc_Program_Choice
+(
 Preference integer NOT NULL,
 Program_Choice_ID integer NOT NULL PRIMARY KEY,
 Application_ID integer NOT NULL FOREIGN KEY REFERENCES nscc_Application(Application_ID),
@@ -133,11 +146,13 @@ CREATE TABLE nscc_Campus_Program(
 Program_ID integer NOT NULL,
 Campus_ID integer NOT NULL
 );
-CREATE TABLE nscc_Credit_Card_Company(
+CREATE TABLE nscc_Credit_Card_Company
+(
 CC_Company_ID integer NOT NULL PRIMARY KEY,
 Company_Name varchar(20) NOT NULL FOREIGN KEY REFERENCES()
 );
-CREATE TABLE nscc_Payment_Credit_Card(
+CREATE TABLE nscc_Payment_Credit_Card
+(
 Payment_ID integer NOT NULL,
 CC_Company_ID integer NOT NULL FOREIGN KEY REFERENCES(),
 Transaction_ID varchar(100) NOT NULL
